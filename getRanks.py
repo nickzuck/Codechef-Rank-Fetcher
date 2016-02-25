3e3777d4debb90e0dcf903c9b11b781199d3f424
nickzuck <nikhil1945.singh@gmail.com>
2016-02-26 03:08:03 +0530

    Done till Printing ..Needs some polishing .......And I need to study something for the midterm ...But not now ;)

--- getRanks.py
+++ getRanks.py
@@ -19,15 +19,31 @@ class Codechef:
     def getRanks(self):
         data = self.data
         for index, value in enumerate(data):
-            html =  0
+            html = (value.values()[0]).read()
+            ranks = self.collectRanks(html)
+            print ranks
+            self.printRanks(value.keys()[0], ranks)
 
 
-    def collectRanks(self):
-        pass
+    def collectRanks(self, html):
+        # Beautiful Soup in Action ... Scraping begins now
+        bsData = BeautifulSoup(html, "html.parser")
+        # Since on codechef the ranks are surrounded by the special tag hx
+        hxData = bsData.findAll("hx") 
+        ranks = []
+        for i in hxData:
+            temp = str(i.string)
+            if temp == 'NA' or temp == 'None':
+                temp = 'NA'
+            ranks.append(temp)
+        return ranks
 
-    def printRanks(self):
-        pass
-
+    def printRanks(self, name, ranks):
+        print " ---------------RANKS FOR %s  are :------------" %(name) 
+        print "Long Contest -- %s | %s" %(ranks[0] , ranks[1])
+        print "Cook Off     -- %s | %s" %(ranks[2], ranks[3])
+        print "Lunch Time   -- %s | %s " %(ranks[4] , ranks[5])
+            
     def getData(self):
         print self.data
 
