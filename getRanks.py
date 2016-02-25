3d6fa1ca684a73603af0bda56de34290bd6c4fc0
nickzuck <nikhil1945.singh@gmail.com>
2016-02-26 02:43:43 +0530

    Fetched Data storage in dictionary .. It's freaking when I realize that I have my exams on the way and I'm doing this shit

--- getRanks.py
+++ getRanks.py
@@ -12,11 +12,25 @@ class Codechef:
         self.getData()
 
     def fetchData(self, name):
-        self.data.append(urllib2.urlopen(self.defaultUrl + str(name)))
+        self.data.append({name : urllib2.urlopen(self.defaultUrl + str(name))})
         print "Data retrived for %s" %(name)
+
+        
+    def getRanks(self):
+        data = self.data
+        for index, value in enumerate(data):
+            html =  0
+
+
+    def collectRanks(self):
+        pass
+
+    def printRanks(self):
+        pass
 
     def getData(self):
         print self.data
 
 
 obj = Codechef(map(str, raw_input().split(" ")))
+obj.getRanks()
